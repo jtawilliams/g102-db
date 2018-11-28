6087aea61a940b3ed9a677640ae003bdb069ec71
jtawilliams <jtawilliams@yahoo.com>
2018-11-27 20:06:24 -0700

    created basic express app with knex file included

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/g102_student_db'
+  },
+
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+
+};
